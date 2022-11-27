9013e50d853d9a2122d172fefb6048adbca2c33e
Anshoool <79800870+AnshulxD@users.noreply.github.com>
2022-11-27 21:25:18 +0530

    Update Main.java
    printing starting function of input reading

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-
+class Main {
+     public static void main(String args[]) throws IOException {
+         InputStreamReader I = new InputStreamReader(System.in);
+         BufferedReader B = new BufferedReader(I);
+         System.out.println("Welcome To MIT Email Services ");
+         System.out.print("Enter function ( signup, login, compose):");
+         String ch = B.readLine();
 
 
 
